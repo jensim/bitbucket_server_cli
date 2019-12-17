3fc4014ef778330b07afc7e35f028ae138393a69
Jens Brimfors <jens.brimfors@nordnet.se>
2019-12-17 16:10:45 +0100

    Output 'e' if error occurs

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,8 @@
-use std::io::{Write, Read};
+use std::io::{ Write};
 use std::path::Path;
-use std::process::{Child, Command};
 use std::result::Result as StdResult;
 
-use generic_error::{GenericError, Result};
+use generic_error::Result;
 use git2::{FetchOptions, RemoteCallbacks};
 use git2::build::RepoBuilder;
 use rayon::prelude::*;
@@ -12,7 +11,7 @@ use crate::types::{Opts, Repo};
 
 pub fn git_going(opts: &Opts, repos: Vec<Repo>) {
     println!("Started working {} repositories", repos.len());
-    println!("Progress: ... c=Clone _=Exists");
+    println!("Progress: ... c=Clone _=Exists e=Err");
     let pool = rayon::ThreadPoolBuilder::new().num_threads(opts.thread_count).build().unwrap();
     let failed: Vec<String> = pool.install(|| {
         repos.into_par_iter().map(|repo| {
@@ -26,19 +25,27 @@ pub fn git_going(opts: &Opts, repos: Vec<Repo>) {
                 let pass = ssh_pass.as_ref().map(|p| p.trim());
                 git2::Cred::ssh_key(user, Some(Path::new(&pub_key)), Path::new(&private_key), pass)
             });
-            match clone_or_update(opts.clone(), &repo, cb) {
-                Ok(result) => None,
-                Err(e) => Some(format!("{}/{} error:{}", repo.project_key, repo.name, e.msg)),
+            match clone_or_update(&repo, cb) {
+                Ok(result) => {
+                    print!("{}", result);
+                    std::io::stdout().flush().unwrap_or(());
+                    None
+                }
+                Err(e) => {
+                    print!("e");
+                    std::io::stdout().flush().unwrap_or(());
+                    Some(format!("{}/{} error:{}", repo.project_key, repo.name, e.msg))
+                }
             }
         }).filter_map(|result: Option<String>| result).collect()
     });
 
     if !failed.is_empty() {
-        eprintln!("{} projects failed to update or clone.", failed.len());
+        eprintln!("\n{} projects failed to update or clone.", failed.len());
         for fail in failed {
             eprintln!("{}", fail);
         }
-    }else {
+    } else {
         println!();
     }
     println!("Done");
@@ -46,21 +53,17 @@ pub fn git_going(opts: &Opts, repos: Vec<Repo>) {
     println!("alias git-pull-recursive='find . -maxdepth 3 -mindepth 2 -type d -name .git -exec sh -c \"cd \\\"{}\\\"/../ && git reset --hard -q && git pull -q --ff-only &\" \\;'", "{}");
 }
 
-fn clone_or_update(opts: &Opts, repo: &Repo, cb: RemoteCallbacks) -> Result<()> {
+fn clone_or_update(repo: &Repo, cb: RemoteCallbacks) -> Result<String> {
     let mut fo = FetchOptions::new();
     fo.remote_callbacks(cb);
 
     match dir_exists(&repo) {
         true => {
-            print!("_");
-            std::io::stdout().flush()?;
-            Ok(())
+            Ok("_".to_string())
         }
         false => {
             git_clone(&repo, fo)?;
-            print!("c");
-            std::io::stdout().flush()?;
-            Ok(())
+            Ok("c".to_string())
         }
     }
 }
