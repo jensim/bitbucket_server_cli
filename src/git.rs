5481e7b0b95ea8679a5daf2315a779f73000f361
Jens Brimfors <jens.brimfors@nordnet.se>
2020-04-23 17:19:25 +0200

    Removed git2 dependency

--- src/git.rs
+++ src/git.rs
@@ -1,10 +1,9 @@
-use std::path::Path;
-use std::process::{Command, Output};
-use std::result::Result as StdResult;
+use std::{
+    path::Path,
+    process::{Command, Output},
+};
 
 use generic_error::Result;
-use git2::{FetchOptions, RemoteCallbacks};
-use git2::build::RepoBuilder;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 use rayon::ThreadPool;
@@ -17,18 +16,18 @@ pub fn git_going(opts: &Opts, repos: Vec<Repo>) {
     bar.set_style(ProgressStyle::default_bar()
         .template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} (eta:{eta})")
         .progress_chars("#>-"));
+    let mut projects: Vec<String> = repos.iter().map(|r| r.project_key.clone()).collect();
+    projects.dedup();
+    projects.iter().for_each(|p| match std::fs::create_dir(p) {
+        Ok(_) => {},
+        Err(ref e) if e.kind() == std::io::ErrorKind::AlreadyExists => {},
+        Err(e) => panic!("{:?}", e),
+    });
+
     let pool: ThreadPool = rayon::ThreadPoolBuilder::new().num_threads(opts.thread_count).build().unwrap();
     let failed: Vec<String> = pool.install(|| {
         repos.into_par_iter().map(|repo| {
-            let mut cb = RemoteCallbacks::new();
-            cb.credentials(|_user: &str, _user_from_url: Option<&str>, _cred: git2::CredentialType, | -> StdResult<git2::Cred, git2::Error> {
-                let user = _user_from_url.unwrap_or("git");
-                let home = std::env::var("HOME").unwrap();
-                let private_key = format!("{}/.ssh/id_rsa", home);
-                let pub_key = format!("{}.pub", private_key);
-                git2::Cred::ssh_key(user, Some(Path::new(&pub_key)), Path::new(&private_key), None)
-            });
-            let result = match clone_or_update(&repo, &opts, cb) {
+            let result = match clone_or_update(&repo, &opts) {
                 Ok(_result) => None,
                 Err(e) => Some(format!("{}/{} error:{}", repo.project_key, repo.name, e.msg))
             };
@@ -46,10 +45,7 @@ pub fn git_going(opts: &Opts, repos: Vec<Repo>) {
     }
 }
 
-fn clone_or_update<'a>(repo: &'a Repo, opts: &Opts, cb: RemoteCallbacks) -> Result<&'a str> {
-    let mut fo = FetchOptions::new();
-    fo.remote_callbacks(cb);
-
+fn clone_or_update<'a>(repo: &'a Repo, opts: &Opts) -> Result<&'a str> {
     match dir_exists(&repo) {
         true => {
             if opts.reset_state {
@@ -58,22 +54,29 @@ fn clone_or_update<'a>(repo: &'a Repo, opts: &Opts, cb: RemoteCallbacks) -> Resu
             Ok(git_update(&repo)?)
         }
         false => {
-            git_clone(&repo, fo)?;
+            git_clone(&repo)?;
             git_reset(repo)?;
             Ok(&"c")
         }
     }
 }
 
-fn git_clone(repo: &Repo, fo: FetchOptions) -> Result<()> {
-    let s = path(&repo);
-    let path = Path::new(&s);
-    RepoBuilder::new().fetch_options(fo).clone(&repo.git, path)?;
-    Ok(())
+fn git_clone(repo: &Repo) -> Result<&str> {
+    let string_path = format!("./{}", repo.project_key);
+    let path = Path::new(&string_path);
+    let out = exec(&*format!("git clone {}", repo.git), path)?;
+
+    let success = out.status.success();
+    return match success {
+        true => Ok("c"),
+        false => Ok("e")
+    }
 }
 
 fn git_update(repo: &Repo) -> Result<&str> {
-    let out = exec("git pull --ff-only", &repo)?;
+    let string_path = path(&repo);
+    let path = Path::new(&string_path);
+    let out = exec("git pull --ff-only", path)?;
     let success = out.status.success();
     let output = format!("{:?}", std::str::from_utf8(out.stdout.as_slice()));
     return if success {
@@ -88,15 +91,15 @@ fn git_update(repo: &Repo) -> Result<&str> {
 }
 
 fn git_reset(repo: &Repo) -> Result<()> {
-    exec("git reset --hard", repo)?;
-    exec("git checkout master --quiet --force --theirs", repo)?;
+    let string_path = path(repo);
+    let path = Path::new(&string_path);
+    exec("git reset --hard", path)?;
+    exec("git checkout master --quiet --force --theirs", path)?;
     Ok(())
 }
 
-fn exec(cmd: &str, repo: &Repo) -> Result<Output> {
+fn exec(cmd: &str, path: &Path) -> Result<Output> {
     let is_win: bool = cfg!(target_os = "windows");
-    let string_path = path(&repo);
-    let path = Path::new(&string_path);
     return match is_win {
         true => {
             Ok(Command::new("cmd").args(&["/C", cmd]).current_dir(path).output()?)
