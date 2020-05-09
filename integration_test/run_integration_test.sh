226353dca7c2cc8683be6e90d6a416de00dc5d54
Jens Brimfors <jens.brimfors@nordnet.se>
2020-05-09 13:37:01 +0200

    CloneType http in integration tests in order to clone on travisCI

--- integration_test/run_integration_test.sh
+++ integration_test/run_integration_test.sh
@@ -17,7 +17,7 @@ cd /tmp
 rm -rf my_hot_repos
 mkdir my_hot_repos
 cd my_hot_repos
-bitbucket_server_cli clone --all --batch --server=http://localhost:8080
+bitbucket_server_cli clone --all --batch --clone-type=http --server=http://localhost:8080
 test -f /tmp/my_hot_repos/active/linuxbrew-bitbucket_server_cli/Formula/bitbucket_server_cli.rb
 test -f /tmp/my_hot_repos/\~jensim/homebrew-bitbucket_server_cli/Formula/bitbucket_server_cli.rb
 
