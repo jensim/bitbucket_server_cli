34e60e5b65ccef3e06d60fdbb1d35b7bd37a206d
Jens Brimfors <jens.brimfors@nordnet.se>
2020-12-22 13:39:48 +0100

    Bump versions

--- src/bitbucket/worker.rs
+++ src/bitbucket/worker.rs
@@ -273,7 +273,13 @@ mod tests {
     use super::*;
 
     fn random_string(len: usize) -> String {
-        thread_rng().sample_iter(&Alphanumeric).take(len).collect()
+        let mut rng = thread_rng();
+        let chars: String = std::iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .map(char::from)
+            .take(len)
+            .collect();
+        chars
     }
 
     fn basic_opts() -> BitBucketOpts {
